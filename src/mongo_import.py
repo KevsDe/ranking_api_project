ef8fb86c80680ca4bd8f5ce68012669bf3c53399
Kevin Careaga <kevin.careaga.bear@gmail.com>
2020-09-23 08:33:10 +0200

    reviewed

--- src/mongo_import.py
+++ src/mongo_import.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
-
+from config import DBURL
 def mongo_import(lista):
     """Upload the info extractred from github to mongodb"""
-    client = MongoClient(port=27017)
+    client = MongoClient(DBURL)
     db=client.datamad0820
     for x in range(0,len(lista)):
         result = db.pull.insert_one(lista[x])
